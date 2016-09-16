82b505575954542cb4bafe993588fb80b210a390
Ami Saunders <write2ami@gmail.com>
2016-09-16 01:08:00 -0700

    first commit

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import weather
+import om_yelp
 import os
 app = Flask(__name__)
 
@@ -8,9 +9,11 @@ app = Flask(__name__)
 def index():
 	address = request.values.get('address')
 	forecast = None
+	top = None
 	if address:
 		forecast = weather.get_weather(address)
-	return render_template('index.html', forecast=forecast)
+		top = om_yelp.top_spots(address)
+	return render_template('index.html', forecast=forecast, top=top)
 
 
 @app.route('/about')
@@ -19,5 +22,6 @@ def about():
 
 
 if __name__ == "__main__":
+	# app.run()
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
